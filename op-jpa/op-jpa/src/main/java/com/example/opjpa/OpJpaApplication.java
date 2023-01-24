165b39e043cc647edc5e994159870b9ee5187d95
alex08-vt <alex.velasquez08@outlook.com>
2023-01-24 11:56:42 -0300

    subiiendo

---
+++ op-jpa/op-jpa/src/main/java/com/example/opjpa/OpJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.opjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpJpaApplication.class, args);
+	}
+
+}
