c17752be0b72db803d83cad6cac3f170a9236953
alex08-vt <alex.velasquez08@outlook.com>
2023-04-01 21:22:29 -0300

    front Angular

---
+++ clinica-odontologica-tp/clinica-odontologica-fix (1)/clinica-front/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+
+  title = 'Clinica Odontologica';
+  subtitle = 'Bienvenido a la clinica odontologica';
+}
