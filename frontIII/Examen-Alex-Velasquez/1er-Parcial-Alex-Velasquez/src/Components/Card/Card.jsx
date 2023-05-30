70061f34c0725c1ac6597b3399118c969da25d20
Alex Velasquez <alex.velasquez08@outlook.com>
2023-05-30 12:01:22 -0300

    form/card

--- frontIII/Examen-Alex-Velasquez/1er-Parcial-Alex-Velasquez/src/Components/Card/Card.jsx
+++ frontIII/Examen-Alex-Velasquez/1er-Parcial-Alex-Velasquez/src/Components/Card/Card.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Form from "../Form/Form";
 
 
 
@@ -6,14 +7,11 @@ import React from "react";
 const Card = () => {
   
     return (
-        <div className="card" style={{width: "18rem"}}>
-        <img src="https://via.placeholder.com/150" className="card-img-top" alt="..." />
-        <div className="card-body">
-            <h5 className="card-title">Nombre Apellido</h5>
-            <p className="card-text">Email</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
-        </div>
-        </div>
+    <div className="card">
+        <h1>Registro</h1>
+       <Form />
+
+    </div>
     );
 };
 
--- frontIII/Examen-Alex-Velasquez/1er-Parcial-Alex-Velasquez/src/Components/Form/Form.jsx
+++ frontIII/Examen-Alex-Velasquez/1er-Parcial-Alex-Velasquez/src/Components/Form/Form.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import './Form.css'
+
 const Form = () => {
   return (
 
@@ -12,31 +14,31 @@ const Form = () => {
 
     //Crear un componente Button.jsx
 <form className="form">
-    <div className="form-group">
+    <div className="form-group etiqueta" >
         <label htmlFor="name">Nombre</label>
-        <input type="text" className="form-control" id="name" placeholder="Nombre" />
+        <input type="text" className="form-control " id="name" placeholder="Nombre" />
     </div>
-    <div className="form-group">
+    <div className="form-group etiqueta">
         <label htmlFor="lastname">Apellido</label>
         <input type="text" className="form-control" id="lastname" placeholder="Apellido" />
     </div>
-    <div className="form-group">
+    <div className="form-group etiqueta">
         <label htmlFor="email">Email</label>
         <input type="email" className="form-control" id="email" placeholder="Email" />
     </div>
-    <div className="form-group">
+    <div className="form-group etiqueta">
         <label htmlFor="password">Contraseña</label>
         <input type="password" className="form-control" id="password" placeholder="Contraseña" />
     </div>
-    <div className="form-group">
+    <div className="form-group etiqueta">
         <label htmlFor="password2">Confirmar Contraseña</label>
         <input type="password" className="form-control" id="password2" placeholder="Confirmar Contraseña" />
     </div>
-    <button type="submit" className="btn btn-primary">Registrarse</button>
+    <button type="submit" className="boton">Registrarse</button>
 
 
 
-    
+
 
 
 </form>
