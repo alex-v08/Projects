759dd181d72731de2182ba250aca3314821329cc
Alex Velasquez <alex.velasquez08@outlook.com>
2023-06-14 15:41:13 -0300

    proyecto mdz

---
+++ projectmdz/src/main/java/com/atuhome/projectmdz/ProjectmdzApplication.java
@@ -0,0 +1,13 @@
+package com.atuhome.projectmdz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectmdzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectmdzApplication.class, args);
+	}
+
+}
---
+++ projectmdz/src/test/java/com/atuhome/projectmdz/ProjectmdzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atuhome.projectmdz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmdzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
